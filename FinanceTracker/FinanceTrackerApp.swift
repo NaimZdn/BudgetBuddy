fefc494d560279fc5f107ac482680bbe3dc8f6d1
Namsco <naim.zidouni@outlook.fr>
2023-03-31 16:10:09 +0200

    Initial Commit

---
+++ FinanceTracker/FinanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTrackerApp.swift
+//  FinanceTracker
+//
+//  Created by Zidouni on 31/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct FinanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
