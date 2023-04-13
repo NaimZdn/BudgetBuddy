e9657618943ad895d0f552d7b5e9063dc7fb81a2
Namsco <naim.zidouni@outlook.fr>
2023-04-13 14:09:39 +0200

    feat(account) : add AccountCreationView

---
+++ BudgetBuddy/AccountCreationView.swift
@@ -0,0 +1,35 @@
+//
+//  AccountCreationView.swift
+//  BudgetBuddy
+//
+//  Created by Zidouni on 13/04/2023.
+//
+
+import SwiftUI
+
+struct AccountCreationView: View {
+    
+    @State private var amount: String = ""
+    
+    var body: some View {
+        HStack{
+            TextField("Ex : 200€", text: $amount)
+                .padding(10)
+                .padding(.horizontal, 10)
+                .keyboardType(.numbersAndPunctuation)
+                .submitLabel(.done)
+            CurrencySelector()
+                .foregroundColor(Color.secondaryColor)
+                .padding(10)
+        }
+        .background(Color.white, in: RoundedRectangle(cornerRadius: 20, style: .continuous))
+        .padding()
+        .background(Color.backgroundColor)
+    }
+}
+
+struct AccountCreationView_Previews: PreviewProvider {
+    static var previews: some View {
+        AccountCreationView()
+    }
+}
