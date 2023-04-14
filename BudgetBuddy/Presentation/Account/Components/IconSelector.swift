2684775e4aa1c8dfba204dcc11b3f8f89b4e389c
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-04-14 09:48:39 +0200

    refactor : refactor the project to MVVM + Clean

---
+++ BudgetBuddy/Presentation/Account/Components/IconSelector.swift
@@ -0,0 +1,20 @@
+//
+//  IconSelector.swift
+//  BudgetBuddy
+//
+//  Created by Zidouni on 14/04/2023.
+//
+
+import SwiftUI
+
+struct IconSelector: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct IconSelector_Previews: PreviewProvider {
+    static var previews: some View {
+        IconSelector()
+    }
+}
