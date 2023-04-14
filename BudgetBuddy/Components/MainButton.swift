e68c3f7d6777160efa868d834aa12f45674a5322
NaimZdn <99655744+NaimZdn@users.noreply.github.com>
2023-04-14 11:33:20 +0200

    feat(main) : add MainButton component

---
+++ BudgetBuddy/Components/MainButton.swift
@@ -0,0 +1,20 @@
+//
+//  MainButton.swift
+//  BudgetBuddy
+//
+//  Created by Zidouni on 14/04/2023.
+//
+
+import SwiftUI
+
+struct MainButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainButton_Previews: PreviewProvider {
+    static var previews: some View {
+        MainButton()
+    }
+}
